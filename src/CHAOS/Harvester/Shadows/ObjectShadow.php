326b75006ae7043ac2f3aa1a4f33fffbbc872aa3
Kræn Hansen <post@creen.dk>
2012-10-31 19:36:59 +0100

    Progressing on the if skipped=true --> unpublish.

--- src/CHAOS/Harvester/Shadows/ObjectShadow.php
+++ src/CHAOS/Harvester/Shadows/ObjectShadow.php
@@ -44,18 +44,6 @@ class ObjectShadow extends Shadow {
 	 */
 	protected $object;
 	
-	/**
-	 * Get the chaos object that this shadow represents.
-	 * @param unknown_type $harvester The Chaos Harvester used to create the shadow.
-	 * @return \stdClass The Chaos object.
-	 */
-	public function getObject($harvester) {
-		if($this->object == null) {
-			$this->object = $this->getOrCreate($harvester);
-		}
-		return $this->object;
-	}
-	
 	public function commit($harvester, $parent = null) {
 		$harvester->debug("Committing the shadow of an object.");
 		if($parent != null) {
@@ -63,30 +51,38 @@ class ObjectShadow extends Shadow {
 		}
 		
 		// Get or create the object, while saving it to the object itself.
-		$this->getObject($harvester);
+		if($this->skipped) {
+			$this->get($harvester, false);
+			if($this->object != null) {
+				// TODO: Implement that this should be unpublished.
+				$harvester->debug("Object with GUID={$this->object->GUID} should be unpublished.");
+			}
+		} else {
+			$this->get($harvester);
 		
-		foreach($this->metadataShadows as $metadataShadow) {
-			$metadataShadow->commit($harvester, $this);
-		}
-		
-		$fileLine = "Committing files: ";
-		foreach($this->fileShadows as $fileShadow) {
-			$file = $fileShadow->commit($harvester, $this);
-			if($file->status == 'reused') {
-				$fileLine .= '.';
-			} else if($file->status == 'created') {
-				$fileLine .= '+';
-			} else {
-				$fileLine .= '?';
+			foreach($this->metadataShadows as $metadataShadow) {
+				$metadataShadow->commit($harvester, $this);
+			}
+			
+			$fileLine = "Committing files: ";
+			foreach($this->fileShadows as $fileShadow) {
+				$file = $fileShadow->commit($harvester, $this);
+				if($file->status == 'reused') {
+					$fileLine .= '.';
+				} else if($file->status == 'created') {
+					$fileLine .= '+';
+				} else {
+					$fileLine .= '?';
+				}
+			}
+			$harvester->info($fileLine);
+			
+			foreach($this->relatedObjectShadows as $relatedObjectShadow) {
+				// TODO: Consider adding a list of committed object shadows to prevent cycles.
+				$relatedObjectShadow->commit($harvester, $this);
 			}
 		}
-		$harvester->info($fileLine);
-		
-		foreach($this->relatedObjectShadows as $relatedObjectShadow) {
-			// TODO: Consider adding a list of committed object shadows to prevent cycles.
-			$relatedObjectShadow->commit($harvester, $this);
-		}
-		// This is sat by the call to getObject.
+		// This is sat by the call to get.
 		return $this->object;
 	}
 	
@@ -94,7 +90,11 @@ class ObjectShadow extends Shadow {
 	 * Get or create the object shadow.
 	 * @param CHAOS\Harvester\ChaosHarvester $harvester
 	 */
-	public function getOrCreate($harvester) {
+	public function get($harvester, $orCreate = true) {
+		if($this->object != null) {
+			return $this->object;
+		}
+		
 		$chaos = $harvester->getChaosClient();
 		
 		// TODO: Consider sorting by DateCreated.
@@ -107,18 +107,22 @@ class ObjectShadow extends Shadow {
 		}
 		$object = null;
 		if($response->MCM()->TotalCount() == 0) {
-			$response = $chaos->Object()->Create($this->objectTypeId, $this->folderId);
-			if(!$response->WasSuccess()) {
-				throw new RuntimeException("General error when creating the object in the chaos service: " . $response->Error()->Message());
-			} elseif(!$response->MCM()->WasSuccess()) {
-				throw new RuntimeException("MCM error when creating the object in the chaos service: " . $response->MCM()->Error()->Message());
-			}
-			if($response->MCM()->TotalCount() == 1) {
-				$results = $response->MCM()->Results();
-				$object = $results[0];
-				$harvester->info("Created a new object in the service with GUID = %s.", $object->GUID);
+			if($orCreate) {
+				$response = $chaos->Object()->Create($this->objectTypeId, $this->folderId);
+				if(!$response->WasSuccess()) {
+					throw new RuntimeException("General error when creating the object in the chaos service: " . $response->Error()->Message());
+				} elseif(!$response->MCM()->WasSuccess()) {
+					throw new RuntimeException("MCM error when creating the object in the chaos service: " . $response->MCM()->Error()->Message());
+				}
+				if($response->MCM()->TotalCount() == 1) {
+					$results = $response->MCM()->Results();
+					$object = $results[0];
+					$harvester->info("Created a new object in the service with GUID = %s.", $object->GUID);
+				} else {
+					throw new RuntimeException("The service didn't respond with a single object when creating it.");
+				}
 			} else {
-				throw new RuntimeException("The service didn't respond with a single object when creating it.");
+				return null;
 			}
 		} else {
 			if($response->MCM()->TotalCount() > 1) {
@@ -129,6 +133,8 @@ class ObjectShadow extends Shadow {
 			$dateCreated = $object->DateCreated;
 			$harvester->info("Reusing object from service, created %s with GUID = %s.", date('r', $dateCreated), $object->GUID);
 		}
-		return $object;
+		
+		$this->object = $object;
+		return $this->object;
 	}
 }
