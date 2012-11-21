461208168545b1a6168646b408b5001f26de27c4
Kræn Hansen <post@creen.dk>
2012-11-21 16:59:33 +0100

    Adding a new abstract mode.

---
+++ src/CHAOS/Harvester/Modes/SetByReferenceMode.php
@@ -0,0 +1,9 @@
+<?php
+namespace CHAOS\Harvester\Modes;
+abstract class SetByReferenceMode extends Mode {
+	/**
+	 * Execute the harvester in the specified mode.
+	 * @param string $reference Some reference to a set of external entities, usually a string or integer.
+	 */
+	public abstract function execute($reference);
+}
