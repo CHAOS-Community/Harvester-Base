121d003532710aae576dd5a70d326267f3702c94
Kræn Hansen <post@creen.dk>
2013-04-08 16:41:03 +0200

    File shadow var_dumps.

--- src/CHAOS/Harvester/Shadows/FileShadow.php
+++ src/CHAOS/Harvester/Shadows/FileShadow.php
@@ -89,8 +89,6 @@ class FileShadow extends Shadow {
 	}
 	
 	protected function matchFile($file) {
-		var_dump($file);
-		var_dump($this);
 		//return $file->URL == $this->URL;
 		if($this->originalFilename != $file->OriginalFilename) {
 			return false;
