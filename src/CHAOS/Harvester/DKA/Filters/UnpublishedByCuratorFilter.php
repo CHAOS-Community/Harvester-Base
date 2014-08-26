261109c5f1c14e5cffdad0a9db248ecd515c140f
madslundt <madssx@hotmail.com>
2014-08-26 13:38:56 +0200

    Added DKA filter (unpublished by curator)

---
+++ src/CHAOS/Harvester/DKA/Filters/UnpublishedByCuratorFilter.php
@@ -0,0 +1,30 @@
+<?php
+namespace CHAOS\Harvester\DKA\Filters;
+
+class UnpublishedByCuratorFilter extends \CHAOS\Harvester\Filters\Filter {
+	public function __construct($harvester, $name, $parameters = array()) {
+		parent::__construct($harvester, $name, $parameters);
+	} 	
+
+	public function passes($externalObject, $objectShadow) {
+		$object = $objectShadow->get($this->_harvester);
+		$metadataSchemes = $object->Metadatas;
+		var_dump($object);
+		exit();
+		foreach ($metadataSchemes as $metadata) {
+
+			// DKA2 metadata scheme.
+			if (strtolower($metadata->MetadataSchemaGUID) === '5906a41b-feae-48db-bfb7-714b3e105396') {
+				$xml = new \SimpleXMLElement($metadata->MetadataXML);
+				if (strval($xml->unpublishedByCurator) === 'true') {
+					return false;
+				}
+				break;
+			}
+		}
+
+		return true;
+	}
+}
+
+?>
