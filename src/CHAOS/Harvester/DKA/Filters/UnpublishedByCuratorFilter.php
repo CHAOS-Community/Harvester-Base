570be36f450c6b2e1727631e0b411071e08409b4
madslundt <madssx@hotmail.com>
2014-08-28 17:14:01 +0200

    Check if object with metadata is empty

--- src/CHAOS/Harvester/DKA/Filters/UnpublishedByCuratorFilter.php
+++ src/CHAOS/Harvester/DKA/Filters/UnpublishedByCuratorFilter.php
@@ -8,17 +8,21 @@ class UnpublishedByCuratorFilter extends \CHAOS\Harvester\Filters\Filter {
 
 	public function passes($externalObject, $objectShadow) {
 		$object = $objectShadow->get($this->_harvester);
-		$metadataSchemes = $object->Metadatas;
-		
-		foreach ($metadataSchemes as $metadata) {
 
-			// DKA2 metadata scheme.
-			if (strtolower($metadata->MetadataSchemaGUID) === '5906a41b-feae-48db-bfb7-714b3e105396') {
-				$xml = new \SimpleXMLElement($metadata->MetadataXML);
-				if (strval($xml->unpublishedByCurator) === 'true') {
-					return false;
+		if (!empty($object) && !empty($object->Metadatas)) {
+			var_dump('test');
+			$metadataSchemes = $object->Metadatas;
+			
+			foreach ($metadataSchemes as $metadata) {
+
+				// DKA2 metadata scheme.
+				if (strtolower($metadata->MetadataSchemaGUID) === '5906a41b-feae-48db-bfb7-714b3e105396') {
+					$xml = new \SimpleXMLElement($metadata->MetadataXML);
+					if (strval($xml->unpublishedByCurator) === 'true') {
+						return false;
+					}
+					break;
 				}
-				break;
 			}
 		}
 
